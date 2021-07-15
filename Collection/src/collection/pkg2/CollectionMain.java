f65c92749c98f9b1ce5a2a641caabfd244d553fc
Sreelakshmi <sreetr1119@gmail.com>
2021-07-15 12:14:49 +0530

    First commit

---
+++ Collection/src/collection/pkg2/CollectionMain.java
@@ -0,0 +1,39 @@
+package collection.pkg2;
+
+import java.util.*;
+
+public class CollectionMain {
+
+	public static void main(String[] args) {
+		
+		ArrayList ar= new ArrayList();
+
+		
+		ar.add(1);//Integer object
+		ar.add("Hello");//String
+      
+        ArrayList<Integer> al=new ArrayList();      
+        al.add(10);
+        al.add(20);
+        
+		System.out.println(ar);
+		
+//		Iterator it= ar.iterator();
+//		
+//		while(it.hasNext()) {
+//			System.out.println(it.next());
+//		}
+//		
+		
+	
+	
+	for(Integer i:al) {
+       System.out.println(i);
+
+	}
+		
+
+	}
+
+}
+
